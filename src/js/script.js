8e570d274c5726cfb1a2c93466f309ce6266f33a
guilherme <guiguicaiano10@gmail.com>
2026-03-30 09:31:39 -0300

    feat: operadores aritiméticos e lógicos

--- src/js/script.js
+++ src/js/script.js
@@ -41,4 +41,27 @@ console.log(exemplo7);
 let exemplo8 =null;
 console.log(exemplo8);
 
+//operadores aritiméticos
+
+const valor1=10;
+const valor2=20;
+
+console.log(valor1 + valor2)
+console.log(valor1 / valor2)
+console.log(valor1 - valor2)
+console.log(valor1 * valor2)
+console.log(valor1 ** valor2)
+
+// operadores logicos
+// = atribuição
+// == comparação
+// === calor e tipo(estritamente igual)
+// && = E (todas condições tem que ser verdadeiras)
+// || = ou (umas das condição tem que ser verdadeira)
+
+console.log(valor1 < valor2);
+console.log(valor1 > 10 && valor2 <10);
+console.log(valor2 > 100 || valor1 <10);
+console.log(valor1 == valor2);
+console.log(valor2 === valor1);
 
