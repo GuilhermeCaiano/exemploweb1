0e1be1592fb4ef50482d7ffc91bab94af75e6ae7
guilherme <guiguicaiano10@gmail.com>
2026-03-30 09:15:37 -0300

    feat: Declarações e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,44 @@
-alert("hello world !!!")
+//Declarações e variaveis
+//var, let e const
+
+var nome="Fiap";
+console.log(nome);
+
+var nome="teste";
+console.log(nome);
+
+if(true){
+    let apelido="fiapinho";
+     console.log(apelido);
+}
+
+const taxaJuros=0.05;
+console.log(taxaJuros);
+
+var exemplo="ola-dev"
+console.log(exemplo); //typeof verifica tipo de variavel colocar por exemplo no atual (typeof exemplo)
+let exemplo1="ola-dev-let"
+console.log(exemplo1);
+const exemplo2="ola-dev-const"
+console.log(exemplo2);
+
+let exemplo3=10 
+console.log(exemplo3);
+
+let exemplo4=true;
+console.log(exemplo4);
+
+let exemplo5={nome1:"teste"}
+console.log(exemplo5);
+
+let exemplo6=["java","c#","PHP"];
+console.log(exemplo6);
+
+//valor undefined
+let exemplo7;
+console.log(exemplo7);
+
+let exemplo8 =null;
+console.log(exemplo8);
+
+
