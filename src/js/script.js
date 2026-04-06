e6bd70d0a350ddb1fd05858f97f3955ca038ad8b
guilherme <guiguicaiano10@gmail.com>
2026-04-06 09:10:47 -0300

    feat:estrutura condicionais

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,178 @@
-//Declarações e variaveis
-//var, let e const
+// //Declarações e variáveis
+// //var, let e const
+// var nome="Fiap";
+// console.log(nome);
 
-const { useDebugValue } = require("react");
+// var nome="Teste";
+// console.log(nome);
 
-var nome="Fiap";
-console.log(nome);
+// if(true){
+//     let apelido="Fiapinho"
+//     console.log(apelido); 
+// }
 
-var nome="teste";
-console.log(nome);
+// const taxaJuros=0.05;
+// console.log(taxaJuros);
+// // taxaJuros=1.5;
+
+// //typeof-verifica o tipo da variavel
+// var exemplo="ola-dev-var";
+// console.log(typeof exemplo)
+
+// let exemplo1="ola-dev-let";
+// console.log(exemplo1)
+
+// const exemplo2="ola-dev-const";
+// console.log(exemplo2)
+
+// let exemplo3=10;
+// console.log(typeof exemplo3)
+
+// let exemplo4=true;
+// console.log (typeof exemplo4);
+
+// let exemplo5={nome1:"teste"};
+// console.log( typeof exemplo5)
+
+// let exemplo6=["Java","C#","PHP"];
+// console.log(exemplo6)
+
+// //valor undefined
+// let exemplo7;
+// console.log(exemplo7)
+
+// //valor vazio
+// let exemplo8 =null;
+// console.log(exemplo8)
+
+// // OPERADORES ARITMÉTICOS
+
+// const valor1=10;
+// const valor2=20;
+
+// console.log( valor1 + valor2);
+// console.log( valor1 - valor2);
+// console.log( valor1 * valor2);
+// console.log( valor1 / valor2);
+
+// //OPERADORES LÓGICOS
+// // = atribuição
+// // == comparação
+// // ===  valor e tipo(estritamente igual)
+// // && = E (todas condições tem que ser verdadeiras)
+// // || = OU (uma das condição tem que ser verdadeira)
+
+// console.log(valor1 < valor2 );
+// console.log(valor1 > 10 && valor2 < 10 );
+// console.log(valor2 < 100 || valor1 >10);
+// console.log(valor1 == valor2);
+// console.log(valor2 === valor1)
+
+
+// /*********************************************
+// *   Exercicio1 
+// *********************************************/
+
+// const p =20;
+// const v=10;
+
+// const precoFinal = p -v;
+// console.log("O preço final com desconto é",precoFinal )
+
+// /*********************************************
+// *   Exercicio2
+// *********************************************/
+
+// let a="20";
+// let b=10;
+
+// let comparar= (a ===20 && b >= 10);
+// console.log("O Resultado é:",comparar);
+
+// /*********************************************
+// *   Exercicio3- Thiago
+// *********************************************/
+
+// var user_login=true;
+// var user_password=true;
+// if(user_login == true){
+//     console.log("Transação Realizada")
+// }
+// /*********************************************
+// *   Exercicio3- cidade
+// *********************************************/
+
+// const usuarioLogado=20;
+// const tempoToken=10;
+
+// const transacao =usuarioLogado ===20 && tempoToken >=10
+// console.log("Transação Autorizada",transacao)
+
+
+//Estrutura condicional
+
+//if
 
 if(true){
-    let apelido="fiapinho";
-     console.log(apelido);
+    console.log("verdadeiro")
 }
 
-const taxaJuros=0.05;
-console.log(taxaJuros);
+let nome="fiap";
 
-var exemplo="ola-dev"
-console.log(exemplo); //typeof verifica tipo de variavel colocar por exemplo no atual (typeof exemplo)
-let exemplo1="ola-dev-let"
-console.log(exemplo1);
-const exemplo2="ola-dev-const"
-console.log(exemplo2);
+if(nome =="fiap"){
+    console.log("nome correto")
+}
 
-let exemplo3=10 
-console.log(exemplo3);
+//if/else
 
-let exemplo4=true;
-console.log(exemplo4);
+if(nome == "fiap"){
+    console.log("usuario correto")
+}else{
+    console.log("usuario incorreto")
+}
 
-let exemplo5={nome1:"teste"}
-console.log(exemplo5);
+// if encadeado/aninhado
 
-let exemplo6=["java","c#","PHP"];
-console.log(exemplo6);
+let idade = 17;
 
-//valor undefined
-let exemplo7;
-console.log(exemplo7);
+if(idade  <=13){
+    console.log("é uma criança")
+}else if(idade >13 && idade <=18){
+    console.log("é um adolescente")
+}else if ( idade >19 && idade <50){
+    console.log("é um adulto")
+}else{
+    console.log("é um idoso")
+}
 
-let exemplo8 =null;
-console.log(exemplo8);
+// Switch Case
 
+let pratos="salada";
 
+switch(pratos){
+    case "macarrao":
+        console.log("a melhor massa")
+        break;
+    case "salada":
+        console.log("salada saudavel")
+        break;
+    case "lasanha":
+        console.log("melhor prato")
+        break;
+    default:
+        console.log("nenhuma das opções")
+}
+
+// ternario
+/*
+? = if
+: = else
+
+*/
+let valor = 100
+let resultado = valor ==100 ? "valor certo":"valor errado";
+console.log(resultado)
+
+let usuario="dev";
+let logado= usuario =="dev"? "Usuario logado":"usuario incorreto";
+console.log(logado)
